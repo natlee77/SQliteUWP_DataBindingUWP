b81dcf819aa753ec57c1a01e68047cde00d4fbc2
natlee77 <natlisjo@gmail.com>
2020-10-22 15:19:45 +0200

    Blob-Storage

---
+++ L_6/StorageAccount_Azure_Blob/StorageAccount_Azure_Blob/Program.cs
@@ -0,0 +1,48 @@
+﻿using StorageAccount_Azure_Blob.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StorageAccount
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=blobazurewin20;AccountKey=YY53jnU4/8Lcr92qy0P3rcHW9X4XZllm7KGRy8eqHx44W6bqJs17Oovab3rr2Os1xaAndi7eGEqC+DcgIEiafQ==;EndpointSuffix=core.windows.net";
+            var containerName = "BLOB";
+
+            Console.WriteLine("Initializing Storage Account with containerName: " + containerName);
+            await StorageService.InitializeStorageAsync(connectionString, containerName);//kan .Awaiter/Get.Awaiter/Task.Run
+
+
+
+            var fileName = $"myfile-{Guid.NewGuid()}.txt";// ska ha unik namn
+            var content = "This is the content of the file";
+            var filePath = Path.Combine(@"D:\Blob\download\", fileName);    
+
+            Console.WriteLine("Creating and writing content in file: " + filePath);
+            await StorageService.WriteToFileAsync(filePath, content);
+
+
+
+            //var downloadPath = Path.Combine(@"d:\WIN20\Downloads\", fileName);
+
+            //Console.WriteLine("Uploading file to Azure Storage Blob in container: " + containerName);
+            //await StorageService.UploadFileAsync(filePath);
+
+            //Console.WriteLine("Downloading file from Azure Storage Blob to: " + Path.GetDirectoryName(downloadPath));
+            //await StorageService.DownloadFileAsync(downloadPath);
+
+            //Console.WriteLine("Reading content from file: " + downloadPath);
+            //Console.WriteLine(await StorageService.ReadDownloadedFileAsync(downloadPath));
+
+
+            //Console.WriteLine("Initializing Storage Account with containerName: " + containerName);
+            //await StorageService.InitializeStorageAsync(connectionString, containerName);
+
+            //Console.WriteLine("Uploading file to Azure Storage Blob in container: " + containerName);
+            //await StorageService.UploadFileAsync(@"d:\WIN20\slide1.jpg");
+        }
+    }
+}
---
+++ L_6/StorageAccount_Azure_Blob/StorageAccount_Azure_Blob/Service/StorageService.cs
@@ -0,0 +1,100 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAccount_Azure_Blob.Services
+{
+    public static class StorageService  //ska ha olika tjänster 
+    {
+        private static BlobContainerClient _containerClient { get; set; }
+        private static BlobClient _blobClient { get; set; }
+
+
+
+        public static async Task InitializeStorageAsync(string connectionString, string containerName, bool uniqueName = false)
+        {
+            if (uniqueName)
+                containerName = $"{containerName}-{Guid.NewGuid()}"; // GUID skapar unik namn till container- varja gång vi generera
+
+            try
+            {
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+                try  // försök skapa container , onm det redan finns en
+                {
+                    _containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+                }
+                catch //då
+                {
+                    try //hämta container som existeras
+                    { 
+                        _containerClient = blobServiceClient.GetBlobContainerClient(containerName); 
+                    }
+                    catch { } // kan ha mdl-- nån gick fel
+                }
+            }
+            catch { }
+        }  //++ i programm
+
+        public static async Task WriteToFileAsync(string @filePath, string content)// skapa fil med namn , och write in file
+        {   // @= D:\Blob\download\
+            //  =D:\\Blob\\download\\
+
+            try // måste ha file directory -wheare file ska ligga
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(filePath)))// OM DET FINNS INTE D:\Blob\download
+                    Directory.CreateDirectory(Path.GetDirectoryName(filePath)); //DÅ SKAPA DIRECTORY TILL
+
+                await File.WriteAllTextAsync(filePath, content);
+            }
+            catch { }
+        }// ++I PROGRAMM
+
+        public static async Task UploadFileAsync(string @filePath)
+        {
+            try
+            {
+                _blobClient = _containerClient.GetBlobClient(Path.GetFileName(filePath));
+
+                using FileStream fileStream = File.OpenRead(filePath);
+                await _blobClient.UploadAsync(fileStream, true);
+                fileStream.Close();
+
+                File.Delete(filePath);
+            }
+            catch { }
+        }//ladda upp fil
+
+        public static async Task DownloadFileAsync(string @downloadPath)
+        {
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(downloadPath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(downloadPath));
+
+                BlobDownloadInfo download = await _blobClient.DownloadAsync();
+
+                using FileStream fileStream = File.OpenWrite(downloadPath);
+                await download.Content.CopyToAsync(fileStream);
+
+                fileStream.Close();
+            }
+            catch { }
+        }
+
+        public static async Task<string> ReadDownloadedFileAsync(string @downloadPath)
+        {
+            try
+            {
+                return await File.ReadAllTextAsync(downloadPath);
+            }
+            catch { }
+
+            return "";
+        }
+    }
+}
